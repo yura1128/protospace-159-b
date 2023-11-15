f6de608581256b0e379c540567f436150d4494ab
iba369963 <148754726+iba369963@users.noreply.github.com>
2023-11-15 21:41:39 +0900

    編集機能の実装＆削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -18,6 +18,25 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path(prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
   def show
     @prototype = Prototype.find(params[:id])
   end
