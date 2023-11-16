bdcecc197acfd1d4b1898c5cfb948449d47201ea
yura1128 <yumeto1128.pr@gmail.com>
2023-11-16 16:34:58 +0900

    コメント機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :prototypes
-  has_many :comments
+  has_many :contents
 
   validates :name, presence: true
   validates :profile, presence: true
