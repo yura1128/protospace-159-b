b62c9ebf40c548a5edb8b660d86f34833c2dbe5f
kumokumokumomo <sky.12.msk@gmail.com>
2023-11-16 17:00:42 +0900

    不適切な値があった場合投稿できずにそのページ留まる修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -14,7 +14,7 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -27,7 +27,7 @@ class PrototypesController < ApplicationController
     if prototype.update(prototype_params)
       redirect_to prototype_path(prototype.id)
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
