f6aadd6970316177b78a2b3fdaa0e4fc8b0a2f74
iba369963 <148754726+iba369963@users.noreply.github.com>
2023-11-16 13:20:07 +0900

    マイページ詳細の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @name = current_user.name
-    @prototypes = current_user.prototypes
+    @prototypes = @user.prototypes
   end
 end
