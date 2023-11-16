6dc4640d327e26e59d0c627bfb57eb1efefed2a6
kumokumokumomo <sky.12.msk@gmail.com>
2023-11-16 18:18:15 +0900

    投稿者以外が編集ページへ遷移できない

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,10 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    if current_user.id != @prototype.user_id 
+      redirect_to root_path
+    end
+    
   end
 
   def update
